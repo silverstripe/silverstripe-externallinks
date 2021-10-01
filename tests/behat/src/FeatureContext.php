3da3567a7a1b23f25eef26316e01e4ba609dea08
Steve Boyd <emteknetnz@gmail.com>
2021-10-01 19:55:27 +1300

    MNT Add behat tests

---
+++ tests/behat/src/FeatureContext.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SilverStripe\ExternalLinks\Tests\Behat\Context;
+
+use SilverStripe\BehatExtension\Context\SilverStripeContext;
+
+class FeatureContext extends SilverStripeContext
+{
+}
---
+++ tests/behat/src/FixtureContext.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SilverStripe\ExternalLinks\Tests\Behat\Context;
+
+use SilverStripe\BehatExtension\Context\FixtureContext as BaseFixtureContext;
+
+class FixtureContext extends BaseFixtureContext
+{
+}
