fb9c340edbfda5bfe0f92a50b7e1427a582b9026
Steve Boyd <emteknetnz@gmail.com>
2021-10-27 18:06:53 +1300

    API phpunit 9 support

--- tests/ExternalLinksTest.php
+++ tests/ExternalLinksTest.php
@@ -22,7 +22,7 @@ class ExternalLinksTest extends SapphireTest
         ExternalLinksTestPage::class
     );
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
